c9251a8a4e54a6aa2e467dc53672ad092f249f11
Marzabayeva Diana <156778435+mewdiaana@users.noreply.github.com>
2024-01-28 17:06:37 +0600

    Create DataTypes.py

---
+++ lab1/Examples/DataTypes.py
@@ -0,0 +1,147 @@
+#1
+x = 5
+print(type(x))
+
+#2
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#3
+x = 20
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#4
+x = 20.5
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#5
+x = 1j
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#6
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#7
+x = ("apple", "banana", "cherry")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#8
+x = range(6)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#9
+x = {"name" : "John", "age" : 36}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#10
+x = {"apple", "banana", "cherry"}
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#11
+x = frozenset({"apple", "banana", "cherry"})
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#12
+x = True
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#13
+x = b"Hello"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#14
+x = bytearray(5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#15
+x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#16
+x = memoryview(bytes(5))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+#17
+x = str("Hello World")
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
