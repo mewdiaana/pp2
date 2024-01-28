6429cb5ef3aa15a0c2ba1983bfa3d393fc7617e3
Marzabayeva Diana <156778435+mewdiaana@users.noreply.github.com>
2024-01-28 16:43:45 +0600

    Create Comments.py

---
+++ lab1/Examples/Comments.py
@@ -0,0 +1,24 @@
+#Comments 1
+#This is a comment
+print("Hello, World!")
+
+#Comments 2
+print("Hello, World!") #This is a comment
+
+#Comments 3
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+#Comments 4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#Comments 5
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
